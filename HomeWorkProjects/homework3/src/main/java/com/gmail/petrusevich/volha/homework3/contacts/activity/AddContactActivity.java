10e8cee9753bd5a42e7a73d8ceec520b7f4b5efc
Olga Petrusevich <volha.petrusevich@gmail.com>
2020-08-25 23:21:45 +0300

    Create listener for RadioGroup.

--- HomeWorkProjects/homework3/src/main/java/com/gmail/petrusevich/volha/homework3/contacts/activity/AddContactActivity.java
+++ HomeWorkProjects/homework3/src/main/java/com/gmail/petrusevich/volha/homework3/contacts/activity/AddContactActivity.java
@@ -7,7 +7,7 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,11 +25,10 @@ public class AddContactActivity extends AppCompatActivity implements View.OnClic
     }
 
     private Button buttonBack;
-    private RadioButton radioButtonPhone;
     private EditText editDataContact;
     private EditText editName;
-    private RadioButton radioButtonMail;
     private Button checkButton;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,6 +36,7 @@ public class AddContactActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_add_contact);
         findView();
         setClick();
+        clickRadioGroup();
     }
 
     @Override
@@ -44,14 +44,6 @@ public class AddContactActivity extends AppCompatActivity implements View.OnClic
         switch (view.getId()) {
             case R.id.back_button:
                 finish();
-                break;
-            case R.id.phone_button:
-                editDataContact.setInputType(InputType.TYPE_CLASS_PHONE);
-                editDataContact.setHint(R.string.phone);
-                break;
-            case R.id.email_button:
-                editDataContact.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
-                editDataContact.setHint(R.string.email);
                 break;
             case R.id.check_button:
                 Intent intent = new Intent();
@@ -64,17 +56,33 @@ public class AddContactActivity extends AppCompatActivity implements View.OnClic
 
     private void findView() {
         buttonBack = findViewById(R.id.back_button);
-        radioButtonPhone = findViewById(R.id.phone_button);
         editDataContact = findViewById(R.id.data_edit);
-        radioButtonMail = findViewById(R.id.email_button);
         checkButton = findViewById(R.id.check_button);
         editName = findViewById(R.id.name_edit_text);
+        radioGroup = findViewById(R.id.group_button);
     }
 
     private void setClick() {
         buttonBack.setOnClickListener(this);
-        radioButtonPhone.setOnClickListener(this);
-        radioButtonMail.setOnClickListener(this);
         checkButton.setOnClickListener(this);
     }
+
+    private void clickRadioGroup() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.phone_button:
+                        editDataContact.setInputType(InputType.TYPE_CLASS_PHONE);
+                        editDataContact.setHint(R.string.phone);
+                        break;
+                    case R.id.email_button:
+                        editDataContact.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                        editDataContact.setHint(R.string.email);
+                        break;
+                }
+            }
+        });
+    }
+
 }
--- HomeWorkProjects/homework3/src/main/java/com/gmail/petrusevich/volha/homework3/contacts/activity/MainActivity.java
+++ HomeWorkProjects/homework3/src/main/java/com/gmail/petrusevich/volha/homework3/contacts/activity/MainActivity.java
@@ -1,15 +1,15 @@
 package com.gmail.petrusevich.volha.homework3.contacts.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import com.gmail.petrusevich.volha.homework3.R;
 import com.gmail.petrusevich.volha.homework3.contacts.adapter.ContactListAdapter;
