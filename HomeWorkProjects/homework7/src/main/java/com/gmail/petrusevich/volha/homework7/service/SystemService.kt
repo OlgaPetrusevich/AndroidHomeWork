fa287ae581a73b0a8b20ac8036087d02038328e9
Olga Petrusevich <volha.petrusevich@gmail.com>
2020-09-28 20:15:37 +0300

    Added notification.

--- HomeWorkProjects/homework7/src/main/java/com/gmail/petrusevich/volha/homework7/service/SystemService.kt
+++ HomeWorkProjects/homework7/src/main/java/com/gmail/petrusevich/volha/homework7/service/SystemService.kt
@@ -1,10 +1,19 @@
 package com.gmail.petrusevich.volha.homework7.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.gmail.petrusevich.volha.homework7.R
 import com.gmail.petrusevich.volha.homework7.logger.Logger
 import com.gmail.petrusevich.volha.homework7.logger.LoggerTextController
+
+private const val CHANNEL_ID = "SYSTEM_CHANNEL"
+private const val TITLE_NOTIFICATION = "System message"
+private const val TEXT_NOTIFICATION = "Logger is working"
 
 class SystemService : Service() {
 
@@ -15,10 +24,23 @@ class SystemService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotification(baseContext)
         val text = LoggerTextController.textLogger
         logger.writeLog(text)
         return super.onStartCommand(intent, flags, startId)
 
+    }
+
+    private fun createNotification(context: Context) {
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+                .setContentTitle(TITLE_NOTIFICATION)
+                .setContentText(TEXT_NOTIFICATION)
+                .build()
+        val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_MIN)
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+        notificationManager.notify(1, notification)
     }
 
 
